a80b2b20c16e249fd5c7441670a20a2bf359c62b
Shohei <shohei.11410723s@gmail.com>
2022-11-25 23:51:42 +0900

    refactored files

--- src/libs/make_dataset.py
+++ src/libs/make_dataset.py
@@ -1,12 +1,16 @@
-import pandas as pd
 import itertools
+import pandas as pd
 import tqdm
+
 AMINO_SET = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K',
              'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'}
 
 
-# protein_sequence_processing returns pd.Series of the number of subsequence of protein.
-# the length of subsequence is length.
+# input:
+#   protein_seq: (str) a sequence of a protein,
+#   longth: (int) the length of a subsequence (default=1)
+# output:
+#   protein_sequence_series: (pd.Series) the series of subsequence frequency of the protein.
 def protein_sequence_processing(protein_seq, length=1):
     protein_sequence_dict = dict()
     for seq in itertools.product(AMINO_SET, repeat=length):
@@ -28,8 +32,11 @@ def protein_sequence_processing(protein_seq, length=1):
     return protein_sequence_series
 
 
-# input dataset: pd.DataFrame, length: the dividing number of amino sequence (default=1)
-# output dataset: pd.Dataframe with the number of amino
+# input:
+#   dataset: (pd.DataFrame) input dataset,
+#   length: (int) the dividing number of amino sequence (default=1)
+# output:
+#   dataset: (pd.Dataframe) dataset with the protein subsequence frequency
 def to_amino_num_features_dataset(dataset: pd.DataFrame, length=1):
     amino_feature = pd.DataFrame()
     for protein_sequence in tqdm.tqdm(dataset['protein_sequence']):
